49d9462ac69dd42fc83b7f808fe35b9fbaecaf85
Rasmus Sterup <69890797+Sterup15@users.noreply.github.com>
2024-09-13 10:48:24 +0200

    2.1 - 2.5 Started on 2.6

---
+++ CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using CLI.UI;
+using InMemoryRepository;
+using Repositories;
+
+namespace CLI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting CLI app...");
+        IUserRepository userRepository = new UserInMemoryRepository();
+        IPostRepository postRepository = new PostInMemoryRepository();
+        ICommentRepository commentRepository = new CommentInMemoryRepository();
+
+        CliApp cliApp = new CliApp(postRepository, userRepository, commentRepository);
+        await cliApp.StartAsync();
+    }
+}
---
+++ CLI/UI/CliApp.cs
@@ -0,0 +1,22 @@
+﻿using Repositories;
+
+namespace CLI.UI;
+
+public class CliApp
+{
+    private IPostRepository PostRepository { get; set; }
+    private IUserRepository UserRepository { get; set; }
+    private ICommentRepository CommentRepository { get; set; }
+
+    public CliApp(IPostRepository postRepository, IUserRepository userRepository, ICommentRepository commentRepository)
+    {
+        PostRepository = postRepository;
+        UserRepository = userRepository;
+        CommentRepository = commentRepository;
+    }
+
+    public async Task StartAsync()
+    {
+        
+    }
+}
